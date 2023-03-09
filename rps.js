debeec50698241e459f629d9ce5862aad4d18fac
Daniel Anestopoulos <dstops97@gmail.com>
2023-03-09 14:43:57 -0500

    Add basic UI

--- rps.js
+++ rps.js
@@ -4,8 +4,44 @@
 
 let userScore = 0;
 let pcScore = 0;
-game();
-displayWinner(userScore,pcScore);
+
+const result = document.getElementById('result');
+const user = document.getElementById('user');
+const pc = document.getElementById('pc');
+const final = document.getElementById('final');
+
+
+
+const rock = document.getElementById('rock');
+rock.addEventListener('click', () => {
+    result.textContent = playRound(rock.id,getComputerChoice())
+});
+
+const paper = document.getElementById('paper');
+paper.addEventListener('click', () => {
+    result.textContent = playRound(paper.id,getComputerChoice())
+});
+
+const scissors = document.getElementById('scissors');
+scissors.addEventListener('click', () => {
+    result.textContent = playRound(scissors.id,getComputerChoice())
+});
+
+const restart = document.getElementById('restart');
+restart.addEventListener('click', () => {
+    rock.removeAttribute("hidden");
+    paper.removeAttribute("hidden");
+    scissors.removeAttribute("hidden");
+    restart.setAttribute("hidden", "hidden");
+    userScore = 0;
+    user.textContent = userScore;
+    pcScore = 0;
+    pc.textContent = pcScore;
+    final.textContent = "";
+    result.textContent = "";
+});
+
+
 
 function getComputerChoice(){
     let randomNum = Math.floor(Math.random() * 3);
@@ -27,50 +63,60 @@ function playRound(playerSelection, computerSelection){
     }
     if (playerSelection == 'rock'){
         if (computerSelection == 'paper'){
-            pcScore++;
+            pc.textContent = ++pcScore;
+            displayWinner(userScore, pcScore)
             return "You Lose! Paper beats Rock!";
         }
         if (computerSelection == 'scissors'){
-            userScore++;
+            user.textContent = ++userScore;
+            displayWinner(userScore, pcScore)
             return "You Win! Rock beats Scissors!";
         }
     }
     if (playerSelection == 'paper'){
         if (computerSelection == 'rock'){
-            userScore++;
+            user.textContent = ++userScore;
+            displayWinner(userScore, pcScore)
             return "You Win! Paper beats Rock!";
         }
         if (computerSelection == 'scissors'){
-            pcScore++;
+            pc.textContent = ++pcScore;
+            displayWinner(userScore, pcScore)
             return "You Lose! Scissors beats Paper!";
         }
     }
     if (playerSelection == 'scissors'){
         if (computerSelection == 'rock'){
-            pcScore++;
+            pc.textContent = ++pcScore;
+            displayWinner(userScore, pcScore)
             return "You Lose! Rock beats Scissors!";
         }
         if (computerSelection == 'paper'){
-            userScore++;
+            user.textContent = ++userScore;
+            displayWinner(userScore, pcScore)
             return "You Win! Scissors beats Paper!";
         }
     }
 }
 
 function displayWinner(userScore, pcScore){
-    if (userScore == pcScore){
-        console.log("Final result is Tie!")
-    } else if (userScore > pcScore){
-        console.log("You are the Winner!")
-    } else {
-        console.log("You are the Loser!")
+    if (userScore == 5 || pcScore == 5){
+        if (userScore == pcScore){
+            final.textContent = "Final result is Tie!"
+        } else if (userScore > pcScore){
+            final.textContent = "You are the Winner!"
+        } else {
+            final.textContent = "You are the Loser!"
+        }
+        hide();
     }
 }
 
-
-function game(){
-    for (let i = 0; i < 5; i++){
-        let userChoice = prompt("Type Rock, Paper or Scissors")
-        console.log(playRound(userChoice,getComputerChoice()));
-    }
+function hide(){
+    rock.setAttribute("hidden", "hidden");
+    paper.setAttribute("hidden", "hidden");
+    scissors.setAttribute("hidden", "hidden");
+    restart.removeAttribute("hidden");
 }
+
+
